52b6c8be112e42a5859f19f0de1e315aa39ae2a0
Mert <mert.izgahi@arneca.com>
2025-03-11 00:45:20 +0300

    tag-api

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,31 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import configs from "./configs";
 import { connectDb } from "./lib/mongoose";
+import { auth, withAuth } from "./lib/auth";
 import { signIn, signUp, signOut } from "./controllers/auth.controller";
+import {
+  createTag,
+  deleteTag,
+  updateTag,
+  getAllTags,
+} from "./controllers/tag.controller";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
-
+app.use(auth);
 const PORT = configs.PORT;
 
 // API routes
 app.post("/api/sign-in", signIn);
 app.post("/api/sign-up", signUp);
 app.post("/api/sign-out", signOut);
+
+app.post("/api/tags", withAuth, createTag);
+app.get("/api/tags", withAuth, getAllTags);
+app.put("/api/tags/:id", withAuth, updateTag);
+app.delete("/api/tags/:id", withAuth, deleteTag);
 
 app.listen(PORT, async () => {
   await connectDb();
